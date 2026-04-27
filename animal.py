a41adecebc3f7bf86a18b5af9b9eff6fc6547635
ivanatf <is728006@iteso.mx>
2026-04-27 15:13:47 -0600

    Inheritance test

---
+++ animal.py
@@ -0,0 +1,21 @@
+class Animal:
+    def __init__(self, name):
+        self.name = name
+
+    def speak(self):
+        raise NotImplementedError("Subclass must implement this method")
+    
+class Dog(Animal):
+    def speak(self):
+        return f"{self.name} says Woof!"
+    
+class Cat(Animal):
+    def speak(self):
+        return f"{self.name} says Meow!"
+    
+# Instances of cat and dog
+cat = Cat("Tommy")
+dog = Dog("Lucas")
+
+print(dog.speak())
+print(cat.speak())
