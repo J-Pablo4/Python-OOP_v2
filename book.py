2a4211b4296e9c412658416aabb4f6d10d3e3ad0
J-Pablo4 <j.pablo.lopez@outlook.com>
2026-04-29 21:47:37 -0600

    Created objects of class

--- book.py
+++ book.py
@@ -3,3 +3,13 @@ class Book:
         self.title = title
         self.author = author
         self.pages = pages
+
+    def display_info(self):
+        return f'Title: {self.title}, Author: \
+            {self.author}, Pages: {self.pages}'
+    
+book1 = Book("1984", "George Orwell", 328)
+book2 = Book("Pride and Prejudice", "Jane Austen", 450)
+
+print(book1.display_info())
+print(book2.display_info())
