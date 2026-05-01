e04e9d5bf71ebca555a6c8c5dc5e3bc70a864a91
J-Pablo4 <j.pablo.lopez@outlook.com>
2026-04-30 21:51:10 -0600

    exercise 1 version 3

--- book_v2.py
+++ book_v2.py
@@ -1,29 +1,43 @@
 class Book:
-    def __init__(self, title, author, year, status="Available"):
+    def __init__(self, title, author, year):
         self.title = title
         self.author = author
         self.year = year
-        self.__status = status
-
-    def book_info(self):
-        return f"Title: {self.title}, Author:{self.author}, Year: {self.year}, Status: {self.__status}"
+        self.__is_checked_out = False
     
-    def rent_book(self):
-        if self.__status == "Available":
-            self.__status = "Rented"
+    def get_status(self):
+        if self.__is_checked_out:
+            return "Checked out"
         else:
-            return f"{self.title} is currently unavailable"
+            return "Available"
+
+    def display_info(self):
+        status = self.get_status()
+        return f"Title: {self.title}, Author:{self.author}, Year: {self.year}, Status: {status}"
+    
+    def check_out(self):
+        if not self.__is_checked_out:
+            self.__is_checked_out = True
+            print(f'You have successfully checked out "{self.title}".')
+        else:
+            print(f'Sorry, "{self.title}" is already checked out.')
         
     def return_book(self):
-        if self.__status == "Rented":
-            self.__status = "Available"
+        if self.__is_checked_out:
+            self.__is_checked_out = False
+            print(f'You have successfully returned "{self.title}".')
         else:
-            return f"The book has already been returned"
+            print(f'"{self.title}" was not checked out.')
         
-book1 = Book("Babel", "R.F. Kuang", "2005")
-print(book1.book_info())
-book1.rent_book()
+book1 = Book("Babel", "R.F. Kuang", 2005)
+book2 = Book("1984","George Orwell", 1949)
+print(book1.display_info())
+print(book2.display_info())
 
-print(book1.book_info())
+book1.check_out()
+print(book1.display_info())
+book1.check_out()
+
 book1.return_book()
-print(book1.book_info())
+print(book1.display_info())
+
