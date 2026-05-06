b50b9cfcd5d984843304abb17dd228dcdf6ffac1
Juan Pablo Lopez <j.pablo.lopez@outlook.com>
2026-05-06 16:39:17 -0600

    Created MagicUser parent class and Mage child class

--- rpg.py
+++ rpg.py
@@ -11,6 +11,14 @@ class Character:
         self.health -= damage
         print(f"{self.name} defends and now has {self.health} health left.")
 
+class MagicUser:
+    def __init__(self, mana):
+        self.mana = mana
+
+    def cast_spell(self):
+        print("Casting a spell...")
+        self.mana -= 10
+
 class Knight(Character):
     def __init__(self, name, health, strength, armor):
         super().__init__(name, health, strength)
@@ -31,7 +39,16 @@ class Archer(Character):
     def attack(self):
         print(f"{self.name} shoots an arrow from {self.range} meters away with strength {self.strength}.")
 
-knight = Knight("Sir rocis", 100, 50, 20)
+class Mage(Character, MagicUser):
+    def __init__(self, name, health, strength, mana):
+        Character.__init__(self, name, health, strength)
+        MagicUser.__init__(self, mana)
+    
+    def attack(self):
+        self.cast_spell()
+        print(f"{self.name} casts a magic attack with strenght {self.strength}!")
+
+knight = Knight("Sir Rose", 100, 50, 20)
 archer = Archer("Robin Hood", 80, 40, 30)
 
 knight.attack()
